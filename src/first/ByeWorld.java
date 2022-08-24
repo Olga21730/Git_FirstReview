80176b6510f31fdd475b7e7c1cc01cbd8f881852
Olga <buruiana.olea@gmail.com>
2022-08-24 17:08:01 -0500

    added ByeWorld.java

---
+++ src/first/ByeWorld.java
@@ -0,0 +1,8 @@
+package first;
+
+public class ByeWorld {
+    public static void main(String[] args) {
+        System.out.println("Maybe 5 weeks ");
+        System.out.println("1.5 weeks of git");
+    }
+}
